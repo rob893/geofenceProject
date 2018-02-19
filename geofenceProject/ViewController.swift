4a114c8be0f76a33a936e79ede203d7d049f37e0
Robert Herber <robertherber@roberts-mac.local>
2018-02-18 16:41:10 -0800

    initial commit

--- geofenceProject/ViewController.swift
+++ geofenceProject/ViewController.swift
@@ -7,19 +7,130 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
+import UserNotifications
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { (granted, error) in }
+        
+        locationManager.delegate = self
+        locationManager.requestAlwaysAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+        
     }
-
+    
+    func postToServerFunction(latitude: Any, longitude: Any){
+        let url:NSURL = NSURL(string: "http://ksucsprojects.com/OtherStuff/GoogleMapsProject/appConnection.php")!
+        let session = URLSession.shared
+        
+        let request = NSMutableURLRequest(url: url as URL)
+        request.httpMethod = "POST"
+        
+        let paramString = "lat=\(latitude)&long=\(longitude)"
+        request.httpBody = paramString.data(using: String.Encoding.utf8)
+        
+        let task = session.dataTask(with: request as URLRequest) { (data, response, error) in
+            guard let _:NSData = data as NSData?, let _:URLResponse = response, error == nil else {
+                print("error")
+                return
+            }
+            
+            if let dataString = NSString(data: data!, encoding: String.Encoding.utf8.rawValue){
+                print(dataString)
+            }
+        }
+        task.resume()
+        
+    }
+    
+   
+    @IBAction func postToServerButton(_ sender: Any) {
+        postToServerFunction(latitude: "1", longitude: "2")
+    }
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
 
+    @IBAction func addRegion(_ sender: Any) {
+        guard let longPress = sender as? UILongPressGestureRecognizer else {
+            return
+        }
+        
+        let touchLocation = longPress.location(in: mapView)
+        let coordinate = mapView.convert(touchLocation, toCoordinateFrom: mapView)
+        let region = CLCircularRegion(center: coordinate, radius: 200, identifier: "geofence")
+        mapView.removeOverlays(mapView.overlays)
+        locationManager.startMonitoring(for: region)
+        let circle = MKCircle(center: coordinate, radius: region.radius)
+        mapView.add(circle)
+        
+    }
+    
+    func showAlert(title: String, message: String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "ok", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func showNotification(title: String, message: String){
+        let content = UNMutableNotificationContent()
+        content.title = title
+        content.body = message
+        content.badge = 1
+        content.sound = .default()
+        let request = UNNotificationRequest(identifier: "notif", content: content, trigger: nil)
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+    }
 }
 
+extension ViewController: CLLocationManagerDelegate{
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        //locationManager.stopUpdatingLocation()
+        mapView.showsUserLocation = true
+        guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
+        postToServerFunction(latitude: locValue.latitude, longitude: locValue.longitude)
+        //print("locations = \(locValue.latitude) \(locValue.longitude)")
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
+        let title = "You entered the region"
+        let message = "penis"
+        showAlert(title: title, message: message)
+        showNotification(title: title, message: message)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
+        let title = "You left the region"
+        let message = "penis"
+        showAlert(title: title, message: message)
+        showNotification(title: title, message: message)    }
+}
+
+extension ViewController: MKMapViewDelegate{
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        guard let circleOverlay = overlay as? MKCircle else {return MKOverlayRenderer() }
+        let circleRenderer = MKCircleRenderer(circle: circleOverlay)
+        circleRenderer.strokeColor = .red
+        circleRenderer.fillColor = .red
+        circleRenderer.alpha = 0.5
+        return circleRenderer
+    }
+}
